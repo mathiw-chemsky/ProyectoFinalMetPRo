e55f91bb9a12b4bbe8be6c2382b941c638918123
Mathiw Chemsky <112981492+mathiw-chemsky@users.noreply.github.com>
2023-11-30 15:26:44 -0600

    Update Estructura del menú (while y switch).cpp

--- CODIGOS/Estructura del menú (while y switch).cpp
+++ CODIGOS/Estructura del menú (while y switch).cpp
@@ -1,56 +1,93 @@
 #include <iostream>
+#include <string.h>
 #include <locale.h>
 using namespace std;
 
+struct ingredientes{
+  string nombre;
+  int cantidad;
+  string proveedor;
+  float precio;
+}ingrediente1, ingrediente2, ingrediente3;
+
+/* void AltaIngredientes(ingredientes ingrediente){
+	ingredientes ingrediente1;
+	
+				cout << "Ingresa el nombre del ingrediente \n";
+				cin >> ingrediente1.nombre;
+				cout << "El nombre de tu producto es: \n" << ingrediente1.nombre << " \n";
+				cout << "Ingresa la cantidad de ese ingrediente \n";
+				cin >> ingrediente1.cantidad;
+				cout << "La cantidad de tu producto es: \n" << ingrediente1.cantidad << " \n";
+				cout << "Ingresa el proveedor de ese ingrediente \n";
+				cin >> ingrediente1.proveedor;
+				cout << "El provedor de tu ingrediente es: \n" << ingrediente1.proveedor << " \n";
+				cout << "Ingresa el precio de ese ingrediente \n";
+				cin >> ingrediente1.precio;
+				cout << "El precio de tu ingrediente es: \n" << ingrediente1.precio << " \n";
+}
+*/
+
 int main (){
-	
-	setlocale(LC_ALL, "");
-	
+
 	int menu = 0;
-	cout << "¡Sea bienvenido al inventario de Chica Pay! \n\n"; 
-	cout << "Menú de opciones: \n";
+	cout << "¡Sea bienvenido al inventario de Chica Pay! \n\n";
+	cout << "Menu de opciones: \n";
 	
 	while (menu != 7){
 		cout << "     1 - Alta de productos \n";
-		cout << "     2 - Modificación de información de productos \n";
-		cout << "     3 - Consulta de catálogo de productos existentes \n";
+		cout << "     2 - Modificacion de informacion de productos \n";
+		cout << "     3 - Consulta de catalogo de productos existentes \n";
 		cout << "     4 - Consulta de existencia productos a partir de una cantidad \n";
 		cout << "     5 - Consulta de proveedores por ingrediente \n";
 		cout << "     6 - Consulta de productos a partir de tu presupuesto \n";
 		cout <<	"     7 - Salir dsel inventario \n\n";
 		
-		cout << "Escriba el número de opción al que desea acceder (solo ingrese números del 1 al 7): \n";
+		cout << "Escriba el numero de opcion al que desea acceder (solo ingrese numeros del 1 al 7): \n";
 		cin >>  menu;
 		
 		switch (menu){
 			case 1:
 				cout << "1 - Alta de productos \n";
-				cout << "Has accedido a la opción de Alta de productos \n";
+				cout << "Has accedido a la opcion de Alta de productos \n";
+//				AltaIngredientes(ingredientes ingrediente);
+				cout << "Ingresa el nombre del ingrediente \n";
+				cin >> ingrediente1.nombre;
+				cout << "El nombre de tu producto es: \n" << ingrediente1.nombre << " \n";
+				cout << "Ingresa la cantidad de ese ingrediente \n";
+				cin >> ingrediente1.cantidad;
+				cout << "La cantidad de tu producto es: \n" << ingrediente1.cantidad << " \n";
+				cout << "Ingresa el proveedor de ese ingrediente \n";
+				cin >> ingrediente1.proveedor;
+				cout << "El provedor de tu ingrediente es: \n" << ingrediente1.proveedor << " \n";
+				cout << "Ingresa el precio de ese ingrediente \n";
+				cin >> ingrediente1.precio;
+				cout << "El precio de tu ingrediente es: \n" << ingrediente1.precio << " \n";
 				break;
 				
 			case 2: 
-				cout << "2 - Modificación de información de productos \n";
-				cout << "Has accedido a la opción de Modificación de información de productos \n";
+				cout << "2 - Modificacion de informacion de productos \n";
+				cout << "Has accedido a la opcion de Modificacion de informacion de productos \n";
 				break;
 			
 			case 3:
-				cout << "3 - Consulta de catálogo de productos existentes \n";
-				cout << "Has accedido a la opción de Consulta de catálogo de productos existentes \n";
+				cout << "3 - Consulta de catalogo de productos existentes \n";
+				cout << "Has accedido a la opcion de Consulta de catÃ¡logo de productos existentes \n";
 				break;
 			
 			case 4:
 				cout << "4 - Consulta de existencia productos a partir de una cantidad \n";
-				cout << "Has accedido a la opción de Consulta de existencia productos a partir de una cantidad \n";
+				cout << "Has accedido a la opcion de Consulta de existencia productos a partir de una cantidad \n";
 				break;
 							
 			case 5:
 				cout << "5 - Consulta de proveedores por ingrediente \n";
-				cout << "Has accedido a la opción de Consulta de proveedores por ingrediente \n";
+				cout << "Has accedido a la opcion de Consulta de proveedores por ingrediente \n";
 				break;
 				
 			case 6:
 				cout << "6 - Consulta de productos a partir de tu presupuesto \n";
-				cout << "Has accedido a la opción de Consulta de productos a partir de tu presupuesto \n";
+				cout << "Has accedido a la opcion de Consulta de productos a partir de tu presupuesto \n";
 				break;
 				
 			case 7:
@@ -58,11 +95,11 @@ int main (){
 				break;
 				
 			default: 
-				cout << "Recuerda escribir solo números del 1 al 7, vuelve a intentar";	
+				cout << "Recuerda escribir solo numeros del 1 al 7, vuelve a intentar";
 		} 
 	}
-	cout << "¡Gracias por acceder al inventario de Chica Pay! \n"; 
-	cout << "¡Hasta pronto!"; 
+	cout << "¡Gracias por acceder al inventario de Chica Pay! \n";
+	cout << "Hasta pronto!";
 	return 0;
 }
 
