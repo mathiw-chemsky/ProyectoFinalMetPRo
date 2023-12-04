3ff1355d09d28976418497fe7f28ff8d44449137
Mathiw Chemsky <112981492+mathiw-chemsky@users.noreply.github.com>
2023-12-03 19:37:08 -0600

    Actualizacion 3 de diciembre
    0.- Menu principal
    0.1.- Modulo registro
    1.- modulo alta ingrediente
    1.1.- modulo crearClave
    2.- modulo modificar ingredientes
    3.- modulo consulta catalogo

--- CODIGOS/2_procedimiento_modificaIngrediente.cpp
+++ CODIGOS/2_procedimiento_modificaIngrediente.cpp
@@ -1,75 +1,32 @@
+/*Módulo para procedimiendo que modifica la información de los ingredientes.
+Entradas: acceso al registro en la posición que se indique, Registro(INGREDIENTES)=nombre, cantidad, proveedor, precio.
+Salidas: Ninguna.
+Proceso: modificar el valor en cada uno de los campos del registro.
+
+1.-Recibe el arreglo de registro INGREDIENTES, y a partir de la clave accedar al registro, para lo cual se imprimirán todos los ingredientes existentes
+numerandolos con un ciclo while y elijirá el numero que quiere modificar, almacenando dicho número en la variable "i".
+2.- Controlar un siwcht dentro de un ciclo while para iterar las veces que el
+usuario desee modificar la información hasta indicarle que para guardar cambios y salir presione
+una tecla distinta del intervalo 1 a 4.
+3.- En el submenú con swicht preguntar qué es lo que desea modificar:
+	1)El nombre, 2) la cantidad, 3) el proveedor, o 4) el precio, 5) salir del submenú y guardar cambios.
+4.- Almacenar los cambios en variables temporales, y cuando la persona decida finalizar mostrar en pantalla si desea guardar los cambios con un IF, para al final almacenarlo
+en el registro.
+*/
+
 #include <iostream>
 #include <string.h>
-#include <locale.h>
-#include <conio.h>
+
 using namespace std;
 
-//MODULO: REGISTRO DE INGREDIENTES--------------------------------------------------------------------------------------------------------------------------
 struct ingredientes{
-  string nombre;    //Decidir si se usan string o  cadena de char nombre[]
+  string nombre;
   float cantidad;
   string proveedor;
   float precio;
   string clave;
 } ingrediente[50];
 
-//PROCEDIMIENTO DE ALTA DE INGREDIENTES-----------------------------------------------------------------------------------------------------------------------
-void AltaIngredientes(ingredientes ingrediente[], int &ingredExistencia){
-	int confirmacion;
-	string nombre;
-	float cantidad;
-	string proveedor;
-	float precio;
-//	char clave;
-
-	//1.- Solicitar informacion al usuario y guardarlo en variables temporales
-	fflush(stdin);
-	cout << "Digite el nombre del ingrediente: ";
-	getline(cin,nombre);
-	cout << "Cantidad del ingrediente: ";
-	cin >> cantidad;
-	fflush(stdin);
-	cout << "Nombre del proveedor del ingrediente: ";
-	getline(cin,proveedor);
-	cout << "Precio del ingrediente: ";
-	cin >> precio;
-	cout << "\n";
-
-	//2.- Imprimir el mensaje de confirmacion
-	cout << "nombre del ingrediente: " << nombre << "\n";
-	cout << "cantidad del ingrediente: " << cantidad << "\n";
- 	cout << "nombre del proveedor: " << proveedor << "\n";
-	cout << "precio del ingrediente: " << precio << "\n";
-    cout << "Si la informacion es correcta presione 1, si la informacion es incorrecta presione cualquier numero \n";
-    cin >> confirmacion;
-
-    //Esto solo es para verificar que no se guardó la información, después de puede borrar
-    if(confirmacion != 1){
- 	cout << "nombre del ingrediente: " << ingrediente[ingredExistencia].nombre << "\n";
-	cout << "cantidad del ingrediente: " << ingrediente[ingredExistencia].cantidad << "\n";
- 	cout << "nombre del proveedor: " << ingrediente[ingredExistencia].proveedor << "\n";
-	cout << "precio del ingrediente: " << ingrediente[ingredExistencia].precio << "\n";
-
-		return;
-    }else{
-	//3.- si es correcto, almacenar las variables temporales en cada campo del registro e ir al paso 4, sino salir del procedimiento sin guardar en el registro nada.
- 	ingrediente[ingredExistencia].nombre = nombre;
-	ingrediente[ingredExistencia].cantidad = cantidad;
-	ingrediente[ingredExistencia].proveedor = proveedor;
-	ingrediente[ingredExistencia].precio = precio;
-	cout << "Informacion almacenada existosamene. \n";
-	ingredExistencia = ingredExistencia + 1;
-	cout << "Posicion: " << ingredExistencia;
-
-	//4.- llamar a la función "creaClave"
-	// AltaIngrediente(proveedor, contador);
-//	clave = 0;
-	//5.- almacenar la clave en el registro
-//	fflush(stdin);
-//	ingrediente[contador].clave = clave << "\n";
-//	cout << "La clave es: " << ingrediente[i].clave << "\n";
-	}
-}
 
 //PROCEDIMIENTO PARA MODIFICAR INGREDIENTES---------------------------------------------------------------------------------------------------------------------
 void ModificaIngredientes(ingredientes ingrediente[], int &ingredExistencia){
@@ -108,8 +65,6 @@ void ModificaIngredientes(ingredientes ingrediente[], int &ingredExistencia){
 		cout << "Escriba el numero de opcion al que desea acceder (solo ingrese numeros del 1 al 5): \n";
 		cin >>  submenu;
 
-	/*3.- En el submenú con swicht preguntar qué es lo que desea modificar:
-		  1)El nombre, 2) la cantidad, 3) el proveedor, o 4) el precio, 5) salir del submenú y guardar cambios.*/
 		switch(submenu){
 			case 1:
 		 	    cout << "------------------------------------------------------------------------------------------------------------------------------------- \n";
@@ -207,93 +162,47 @@ void ModificaIngredientes(ingredientes ingrediente[], int &ingredExistencia){
 		}
 
 	}
+
+
+	/*3.- En el submenú con swicht preguntar qué es lo que desea modificar:
+		  1)El nombre, 2) la cantidad, 3) el proveedor, o 4) el precio, 5) salir del submenú y guardar cambios.
+	cout << "------------------------------------------------------------------------------------------------------------------------------------- \n";
+	cout << "Usted ingreso la siguiente informacion: \n";
+	cout << "nombre del ingrediente: " << nombre << "\n";
+	cout << "cantidad del ingrediente: " << cantidad << "\n";
+ 	cout << "nombre del proveedor: " << proveedor << "\n";
+	cout << "precio del ingrediente: " << precio << "\n";
+    cout << " ¿Esta seguro de que desea guardar los cambios? Si la informacion es correcta y desea guardar, presione 1. \n";
+	cout << "Si la informacion es incorrecta y no desea guardar presione cualquier numero \n";
+    cin >> confirmacion;
+
+
+    if(confirmacion != 1){
+    //Esto solo es para verificar que no se guardó la información, después de puede borrar
+ 	cout << "nombre del ingrediente: " << ingrediente[i].nombre << "\n";
+	cout << "cantidad del ingrediente: " << ingrediente[i].cantidad << "\n";
+ 	cout << "nombre del proveedor: " << ingrediente[i].proveedor << "\n";
+	cout << "precio del ingrediente: " << ingrediente[i].precio << "\n";
+		return;
+    }else{
+	/* 4.- Almacenar los cambios en variables temporales, y cuando la persona decida finalizar mostrar en pantalla si desea guardar los cambios con un IF,
+	para al final almacenarlo en el registro.
+ 	ingrediente[i].nombre = nombre;
+	ingrediente[i].cantidad = cantidad;
+	ingrediente[i].proveedor = proveedor;
+	ingrediente[i].precio = precio;
+	cout << "Informacion almacenada exitosamente. \n";
+	cout << "\n";
+	} */
 }
 
-//PROCEDIMIENTO PARA CONSULTA DE CATALOGO
-void consultaCatalogo(ingredientes ingrediente[], int ingredExistencia){
 
-	 cout << "Catalogo de ingredientes existentes. \n";
-	 cout << "--------------------------------------------------------------------------------------------------------------------------------------------\n";
-
-	 for(int i = 0; i < ingredExistencia; i++){
-	    cout << "nombre del ingrediente: " << ingrediente[i].nombre << "\n";
-		cout << "cantidad del ingrediente: " << ingrediente[i].cantidad << "\n";
- 		cout << "nombre del proveedor: " << ingrediente[i].proveedor << "\n";
-		cout << "precio del ingrediente: " << ingrediente[i].precio << "\n";
-		cout << "-----------------------------------------------------------------------------------------\n";
-	 }
-}
-
-//FUNCION PRINCIPAL-----------------------------------------------------------------------------------------------------------------------------------------
-int main (){
-//	struct ingredientes ingrediente1;
-    int menu = 0;
+int main(){
 	int ingredExistencia= 0;
-	
-	cout << "¡Sea bienvenido al inventario de Chica Pay! \n\n";
 
-	
-	while (menu != 7){
- 	    cout << "------------------------------------------------------------------------------------------------------------------------------------- \n";
-		cout << "Menu de opciones: \n";
-		cout << "     1 - Alta de productos \n";
-		cout << "     2 - Modificacion de informacion de productos \n";
-		cout << "     3 - Consulta de catalogo de productos existentes \n";
-		cout << "     4 - Consulta de existencia productos a partir de una cantidad \n";
-		cout << "     5 - Consulta de proveedores por ingrediente \n";
-		cout << "     6 - Consulta de productos a partir de tu presupuesto \n";
-		cout <<	"     7 - Salir dsel inventario \n\n";
-		
-		cout << "Escriba el numero de opcion al que desea acceder (solo ingrese numeros del 1 al 7): \n";
-		cin >>  menu;
-		cout << "------------------------------------------------------------------------------------------------------------------------------------- \n";
+	ModificaIngredientes(ingrediente, ingredExistencia);
 
-		
-		switch (menu){
-			case 1:
-				cout << "1 - Alta de productos \n";
-				cout << "Has accedido a la opcion de Alta de productos \n";
-				AltaIngredientes(ingrediente, ingredExistencia);
-				cout << "\n Posicion actual: " << ingredExistencia;
-				break;
-				
-			case 2: 
-				cout << "2 - Modificacion de informacion de productos \n";
-				cout << "Has accedido a la opcion de Modificacion de informacion de productos \n";
-				ModificaIngredientes(ingrediente, ingredExistencia);
-				break;
-			
-			case 3:
-				cout << "3 - Consulta de catalogo de productos existentes \n";
-				cout << "Has accedido a la opcion de Consulta de catalogo de productos existentes \n";
-				consultaCatalogo(ingrediente, ingredExistencia);
-				break;
-			
-			case 4:
-				cout << "4 - Consulta de existencia productos a partir de una cantidad \n";
-				cout << "Has accedido a la opcion de Consulta de existencia productos a partir de una cantidad \n";
-				break;
-							
-			case 5:
-				cout << "5 - Consulta de proveedores por ingrediente \n";
-				cout << "Has accedido a la opcion de Consulta de proveedores por ingrediente \n";
-				break;
-				
-			case 6:
-				cout << "6 - Consulta de productos a partir de tu presupuesto \n";
-				cout << "Has accedido a la opcion de Consulta de productos a partir de tu presupuesto \n";
-				break;
-				
-			case 7:
-				cout << "7 - Has salido del inventario \n\n";
-				break;
-				
-			default: 
-				cout << "Recuerda escribir solo numeros del 1 al 7, vuelve a intentar";
-		} 
-	}
-	cout << "¡Gracias por acceder al inventario de Chica Pay! \n";
-	cout << "Hasta pronto!";
+
+
 	return 0;
 }
-
--- CODIGOS/procedimiento_altaIngrediente.cpp
+++ CODIGOS/procedimiento_altaIngrediente.cpp
@@ -1,9 +1,10 @@
 /*Módulo de procedimiento de alta de producto
-1.- almacenar toda la informacion en registro
+1.- Solicitar informacion al usuario y guardarlo en variables temporales
 2.- imprimir la información para preguntar si es correcto
-3.- contador sumar en 1
-4.- llamar a funcion de creacion de clave
-5.- almacenar la clave en el registro
+3.- si es correcto, almacenar las variables temporales en cada campo del registro e ir al paso 4, sino salir del procedimiento sin guardar en el registro nada.
+4.- contador sumar en 1
+5.- llamar a funcion de creacion de clave
+6.- almacenar la clave en el registro
 */
 #include <iostream>
 #include <string.h>
@@ -18,53 +19,72 @@ struct ingredientes{
   string clave;
 } ingrediente[50];
 
-void AltaIngredientes(ingredientes ingrediente[]){
-	int contador = 0;
+
+void AltaIngredientes(ingredientes ingrediente[], int &ingredExistencia){
 	int confirmacion;
-	char clave;
-	
-	//1.- Almacenar la informacion en el registro
+	string nombre;
+	float cantidad;
+	string proveedor;
+	float precio;
+//	char clave;
+
+	//1.- Solicitar informacion al usuario y guardarlo en variables temporales
 	fflush(stdin);
 	cout << "Digite el nombre del ingrediente: ";
-	getline(cin,ingrediente[contador].nombre);
+	getline(cin,nombre);
 	cout << "Cantidad del ingrediente: ";
-	cin >> ingrediente[contador].cantidad;
+	cin >> cantidad;
 	fflush(stdin);
 	cout << "Nombre del proveedor del ingrediente: ";
-	getline(cin,ingrediente[contador].proveedor);
+	getline(cin,proveedor);
 	cout << "Precio del ingrediente: ";
-	cin >> ingrediente[contador].precio;
+	cin >> precio;
 	cout << "\n";
 
 	//2.- Imprimir el mensaje de confirmacion
-	cout << "nombre del ingrediente: " << ingrediente[contador].nombre << "\n";
-	cout << "cantidad del ingrediente: " << ingrediente[contador].cantidad << "\n";
- 	cout << "nombre del proveedor: " << ingrediente[contador].proveedor << "\n";
-	cout << "precio del ingrediente: " << ingrediente[contador].precio << "\n";
+	cout << "nombre del ingrediente: " << nombre << "\n";
+	cout << "cantidad del ingrediente: " << cantidad << "\n";
+ 	cout << "nombre del proveedor: " << proveedor << "\n";
+	cout << "precio del ingrediente: " << precio << "\n";
     cout << "Si la informacion es correcta presione 1, si la informacion es incorrecta presione cualquier numero \n";
     cin >> confirmacion;
-    
+
+    //Esto solo es para verificar que no se guardó la información, después de puede borrar
     if(confirmacion != 1){
-    	return;
+ 	cout << "nombre del ingrediente: " << ingrediente[ingredExistencia].nombre << "\n";
+	cout << "cantidad del ingrediente: " << ingrediente[ingredExistencia].cantidad << "\n";
+ 	cout << "nombre del proveedor: " << ingrediente[ingredExistencia].proveedor << "\n";
+	cout << "precio del ingrediente: " << ingrediente[ingredExistencia].precio << "\n";
+
+		return;
     }else{
-	//3.- aumentar el contador en 1
-	contador = contador + 1;
+	//3.- si es correcto, almacenar las variables temporales en cada campo del registro e ir al paso 4, sino salir del procedimiento sin guardar en el registro nada.
+ 	ingrediente[ingredExistencia].nombre = nombre;
+	ingrediente[ingredExistencia].cantidad = cantidad;
+	ingrediente[ingredExistencia].proveedor = proveedor;
+	ingrediente[ingredExistencia].precio = precio;
+	cout << "Informacion almacenada existosamene. \n";
+	ingredExistencia = ingredExistencia + 1;
+	cout << "Posicion: " << ingredExistencia;
 
 	//4.- llamar a la función "creaClave"
 	// AltaIngrediente(proveedor, contador);
-	clave = 0;
+//	clave = 0;
 	//5.- almacenar la clave en el registro
 //	fflush(stdin);
 //	ingrediente[contador].clave = clave << "\n";
-	cout << "La clave es: " << ingrediente[contador].clave << "\n";
+//	cout << "La clave es: " << ingrediente[i].clave << "\n";
 	}
 }
 
 
 int main(){
-	int contador;
+	int ingredExistencia= 0;
 
-	AltaIngredientes(struct ingredientes ingrediente);
+	AltaIngredientes(ingrediente, ingredExistencia);
+	cout << "\n Posicion actual: " << ingredExistencia;
+	
+
 
 	return 0;
 }
